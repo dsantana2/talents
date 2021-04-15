ffbb7b80a99bd36948efc0f8f1db19585f174152
Darren Santana <darrensantana@ip-10-0-0-164.us-west-2.compute.internal>
2021-04-15 14:07:16 -0400

    fix

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
   res.send("Welcome To The Home Pagee");
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Port 3000");
 });
